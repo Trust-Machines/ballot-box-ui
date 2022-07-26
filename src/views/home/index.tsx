d6555e7750703848cd4874ad50a2ebad24f816bd
talhasch <talhabugrabulut@gmail.com>
2022-07-26 15:06:01 +0300

    save

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {RouteComponentProps} from '@reach/router';
 
 import AppMenu from '../../layout/app-menu';
-import PageHeader from '../../layout/app-header';
+import AppHeader from '../../layout/app-header';
 import AppContent from '../../layout/app-content';
 
 const Home = (_: RouteComponentProps) => {
     return <>
         <AppMenu/>
         <AppContent>
-            <PageHeader/>
+            <AppHeader/>
         </AppContent>
     </>
 }
