7a7b06df5f3de636b68e04f426646251e24431ba
talhasch <talhabugrabulut@gmail.com>
2022-07-26 15:39:26 +0300

    save

--- src/components/themed-box/index.tsx
+++ src/components/themed-box/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Box from '@mui/material/Box';
-import {grey} from '@mui/material/colors';
 import {useTheme} from '@mui/material';
 import {SxProps} from '@mui/system';
 import {Theme} from '@mui/material/styles';
@@ -12,10 +11,9 @@ const ThemedBox = (props: { children: React.ReactNode, sx?: SxProps<Theme> }) =>
     return <Box sx={
         {
             ...{
-                bgcolor: theme.palette.mode === 'light' ? '#fff' : grey[800],
-                padding: '10px',
-                borderRadius: '6px',
-                boxShadow: `${theme.palette.divider} 1px 2px 10px 0px`
+                border: `1px solid ${theme.palette.divider}`,
+                padding: '16px',
+                borderRadius: '12px',
             },
             ...props.sx
         }
