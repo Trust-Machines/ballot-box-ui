8b9d7b3b6336a13b90ba243e57e61c01a2984991
talhasch <talhabugrabulut@gmail.com>
2022-07-26 16:46:30 +0300

    save

--- src/views/create-space/index.tsx
+++ src/views/create-space/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Box from '@mui/material/Box';
 import {RouteComponentProps} from '@reach/router';
 import {Helmet} from 'react-helmet';
@@ -15,6 +15,18 @@ import ThemedBox from '../../components/themed-box';
 
 const CreateSpace = (_: RouteComponentProps) => {
     const [t] = useTranslation();
+
+    const [name, setName] = useState('');
+    const [about, setAbout] = useState('');
+    const [website, setWebsite] = useState('');
+    const [terms, setTerms] = useState('');
+    const [twitter, setTwitter] = useState('');
+    const [github, setGithub] = useState('');
+
+    const submit = () => {
+
+    }
+
     return <>
         <Helmet><title>{t('Create a space')}</title></Helmet>
         <AppMenu/>
@@ -25,30 +37,30 @@ const CreateSpace = (_: RouteComponentProps) => {
                 <ThemedBox>
                     <Box>
                         <TextField label={t('Space name')} inputProps={{maxLength: 30}}
-                                   fullWidth autoFocus autoComplete="off"/>
+                                   fullWidth autoFocus autoComplete="off" value={name}/>
                     </Box>
                     <Box sx={{mt: '12px'}}>
                         <TextField label={t('About')} inputProps={{maxLength: 250}}
-                                   fullWidth autoFocus autoComplete="off"/>
+                                   fullWidth autoFocus autoComplete="off" value={about}/>
                     </Box>
                     <Box sx={{mt: '12px'}}>
                         <TextField label={t('Website')} placeholder="https://superdao.com" inputProps={{maxLength: 150}}
-                                   fullWidth autoFocus autoComplete="off"/>
+                                   fullWidth autoFocus autoComplete="off" value={website}/>
                     </Box>
                     <Box sx={{mt: '12px'}}>
                         <TextField label={t('Terms')} placeholder="https://forum.superdao.com/terms"
                                    inputProps={{maxLength: 150}}
-                                   fullWidth autoFocus autoComplete="off"/>
+                                   fullWidth autoFocus autoComplete="off" value={terms}/>
                     </Box>
                     <Box sx={{mt: '12px'}}>
                         <TextField label={t('Twitter')} placeholder="superdao"
                                    inputProps={{maxLength: 40}}
-                                   fullWidth autoFocus autoComplete="off"/>
+                                   fullWidth autoFocus autoComplete="off" value={twitter}/>
                     </Box>
                     <Box sx={{mt: '12px'}}>
                         <TextField label={t('Github')} placeholder="superdao_code"
                                    inputProps={{maxLength: 40}}
-                                   fullWidth autoFocus autoComplete="off"/>
+                                   fullWidth autoFocus autoComplete="off" value={github}/>
                     </Box>
                     <Box sx={{mt: '12px', display: 'flex', justifyContent: 'center'}}>
                         <Button variant="contained">Create</Button>
