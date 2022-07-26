75eb8f55f9d9133fcd873158f7bb5ee6af5c5537
talhasch <talhabugrabulut@gmail.com>
2022-07-26 16:29:24 +0300

    save

--- src/layout/app-wrapper/index.tsx
+++ src/layout/app-wrapper/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box} from '@mui/material';
+import Box from '@mui/material/Box';
 
 const AppWrapper = (props: { children: React.ReactNode }) => {
     const {children} = props;
--- src/views/create-space/index.tsx
+++ src/views/create-space/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import {RouteComponentProps} from '@reach/router';
 import {Helmet} from 'react-helmet';
-
+import TextField from '@mui/material/TextField';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
 import AppMenu from '../../layout/app-menu';
 import AppHeader from '../../layout/app-header';
 import AppWrapper from '../../layout/app-wrapper';
@@ -21,7 +23,36 @@ const CreateSpace = (_: RouteComponentProps) => {
             <AppContent>
                 <Box sx={{fontSize: '26px', fontWeight: '600', mb: '20px'}}>{t('Create a space')}</Box>
                 <ThemedBox>
-                    dd
+                    <Box>
+                        <TextField label={t('Space name')} inputProps={{maxLength: 30}}
+                                   fullWidth autoFocus autoComplete="off"/>
+                    </Box>
+                    <Box sx={{mt: '12px'}}>
+                        <TextField label={t('About')} inputProps={{maxLength: 250}}
+                                   fullWidth autoFocus autoComplete="off"/>
+                    </Box>
+                    <Box sx={{mt: '12px'}}>
+                        <TextField label={t('Website')} placeholder="https://superdao.com" inputProps={{maxLength: 150}}
+                                   fullWidth autoFocus autoComplete="off"/>
+                    </Box>
+                    <Box sx={{mt: '12px'}}>
+                        <TextField label={t('Terms')} placeholder="https://forum.superdao.com/terms"
+                                   inputProps={{maxLength: 150}}
+                                   fullWidth autoFocus autoComplete="off"/>
+                    </Box>
+                    <Box sx={{mt: '12px'}}>
+                        <TextField label={t('Twitter')} placeholder="superdao"
+                                   inputProps={{maxLength: 40}}
+                                   fullWidth autoFocus autoComplete="off"/>
+                    </Box>
+                    <Box sx={{mt: '12px'}}>
+                        <TextField label={t('Github')} placeholder="superdao_code"
+                                   inputProps={{maxLength: 40}}
+                                   fullWidth autoFocus autoComplete="off"/>
+                    </Box>
+                    <Box sx={{mt: '12px', display: 'flex', justifyContent: 'center'}}>
+                        <Button variant="contained">Create</Button>
+                    </Box>
                 </ThemedBox>
             </AppContent>
         </AppWrapper>
