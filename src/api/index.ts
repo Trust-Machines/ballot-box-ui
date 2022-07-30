bd2d7476d0d5567739d1e46d69b2ceb51d7a99ae
talhasch <talhabugrabulut@gmail.com>
2022-07-30 12:58:56 +0300

    save

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,18 +11,17 @@ class ApiError extends Error {
     }
 }
 
-export const apiPost = <T>(auth: USER_AUTH, network: NETWORK, endpoint: string, body: {}): Promise<T> => {
-    return fetch(`${API_BASE}/${endpoint}`, {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-            'x-signature': auth.signature,
-            'x-public-key': auth.publicKey,
-            'x-network': network
-        }
-    })
-        .then(r => r.json())
+const makeApiHeaders = (auth: USER_AUTH, network: NETWORK) => {
+    return {
+        'Content-type': 'application/json; charset=UTF-8',
+        'x-signature': auth.signature,
+        'x-public-key': auth.publicKey,
+        'x-network': network
+    }
+}
+
+const handleApiResponse = (promise: Promise<Response>) => {
+    return promise.then(r => r.json())
         .then(r => {
             if (r.error) {
                 throw new ApiError(r.error);
@@ -30,6 +29,25 @@ export const apiPost = <T>(auth: USER_AUTH, network: NETWORK, endpoint: string, 
 
             return r;
         });
+}
+
+export const apiPost = <T>(auth: USER_AUTH, network: NETWORK, endpoint: string, body: {}): Promise<T> => {
+    return handleApiResponse(fetch(`${API_BASE}/${endpoint}`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: makeApiHeaders(auth, network)
+    }));
+}
+
+export const apiGet = <T>(auth: USER_AUTH, network: NETWORK, endpoint: string): Promise<T> => {
+    return handleApiResponse(fetch(`${API_BASE}/${endpoint}`, {
+        method: 'GET',
+        headers: makeApiHeaders(auth, network)
+    }));
+}
+
+export const getUserSpaces = (auth: USER_AUTH, network: NETWORK): Promise<Space[]> => {
+    return apiGet(auth, network, 'spaces');
 }
 
 export const createSpace = (auth: USER_AUTH, network: NETWORK, name: string): Promise<Space> => {
