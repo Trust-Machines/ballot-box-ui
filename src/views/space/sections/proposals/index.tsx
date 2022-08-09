5cecd7026e33f4fb9691dc4db01e143a7fa253bc
talhasch <talhabugrabulut@gmail.com>
2022-08-09 17:45:37 +0300

    save

--- src/views/space/sections/proposals/index.tsx
+++ src/views/space/sections/proposals/index.tsx
@@ -1,10 +1,11 @@
 import {Space} from '../../../../types';
 import {Muted} from '../../../../components/text';
+import useTranslation from '../../../../hooks/use-translation';
 
 const Proposals = (props: { space: Space }) => {
-
+    const [t] = useTranslation();
     return <>
-        <Muted sx={{textAlign: 'center', mt: '100px'}}>No proposals yet</Muted>
+        <Muted sx={{textAlign: 'center', mt: '100px'}}>{t('There\'s nothing here, yet.')}</Muted>
     </>;
 }
 
