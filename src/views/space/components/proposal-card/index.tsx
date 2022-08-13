dd8d62aba4dae1a0ceb0a0a17602b13730033617
talhasch <talhabugrabulut@gmail.com>
2022-08-13 10:31:59 +0300

    proposal card

--- src/views/space/components/proposal-card/index.tsx
+++ src/views/space/components/proposal-card/index.tsx
@@ -1,7 +1,25 @@
+import {Box, useTheme} from '@mui/material';
+
+import {H3, Muted} from '../../../../components/text'
 import {Proposal} from '../../../../types';
 
-const ProposalCard = (props: {proposal: Proposal}) => {
-    return null;
+
+const ProposalCard = (props: { proposal: Proposal }) => {
+    const {proposal} = props;
+    const theme = useTheme();
+
+    return <Box sx={{
+        border: `1px solid ${theme.palette.divider}`,
+        padding: '18px',
+        borderRadius: '8px',
+        mb: '20px',
+        '&:hover': {}
+    }}>
+        <H3>{proposal.title}</H3>
+        <Muted sx={{
+            mb: '20px'
+        }}>{proposal.body}</Muted>
+    </Box>
 }
 
 export default ProposalCard;
