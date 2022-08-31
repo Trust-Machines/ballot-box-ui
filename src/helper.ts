4f64d59fdc8eff3f93a5f96178acac13a0ecdad1
talhasch <talhabugrabulut@gmail.com>
2022-08-31 15:36:17 +0300

    trigger re-deploy

--- src/helper.ts
+++ src/helper.ts
@@ -3,7 +3,6 @@ import numeral from 'numeral';
 import {NETWORK} from './types';
 import {NETWORKS} from './constants';
 
-
 export const getStacksNetwork = (n: NETWORK): StacksNetwork => {
     return NETWORKS[n];
 }
