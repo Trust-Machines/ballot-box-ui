fdca5216bcfdae1e3053400edcddc12d9500d50f
talhasch <talhabugrabulut@gmail.com>
2022-09-20 13:49:53 +0300

    save

--- src/api/stacks.ts
+++ src/api/stacks.ts
@@ -10,7 +10,17 @@ export const getBnsName = (address: string): Promise<string | null> => {
 }
 
 export const getBlock = (height: number): Promise<{
-    hash: string
+    height: number,
+    hash: string,
+    index_block_hash: string
 }> => {
     return fetch(`${NETWORKS['mainnet'].coreApiUrl}/extended/v1/block/by_height/${height}`).then(r => r.json());
+}
+
+export const getCurrentBlock = (): Promise<{
+    height: number
+}> => {
+    return fetch(`${NETWORKS['mainnet'].coreApiUrl}/extended/v1/block`)
+        .then(r => r.json())
+        .then(r => r.results[0])
 }
