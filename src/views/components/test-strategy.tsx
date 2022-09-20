e37177a41fd6e39751c7d8ba4fb4ebca195a5358
talhasch <talhabugrabulut@gmail.com>
2022-09-20 14:10:07 +0300

    test dialog improvements

--- src/views/components/test-strategy.tsx
+++ src/views/components/test-strategy.tsx
@@ -6,27 +6,23 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
 import {runStrategy} from '@trustmachines/ballot-box-strategies';
+import {validateStacksAddress} from '@stacks/transactions';
 
 import CloseModal from '../../components/close-modal';
 import ThemedBox from '../../components/themed-box';
-import {Muted} from '../../components/text';
 import useAddress from '../../hooks/use-address';
 import useModal from '../../hooks/use-modal';
-import useToast from '../../hooks/use-toast';
 import useTranslation from '../../hooks/use-translation';
-import useMediaBreakPoint from '../../hooks/use-media-break-point';
-import {vote} from '../../api/ballot-box';
-import {NETWORK, ProposalWithSpace, SpaceBase, Space, USER_AUTH, VoteWithProposal} from '../../types';
-import {NETWORKS} from '../../constants';
-import {truncateMiddle} from '../../util';
+import useToast from '../../hooks/use-toast';
 import {formatVotePower} from '../../helper';
-
 import {getCurrentBlock, getBlock} from '../../api/stacks';
-
+import {NETWORK, Space} from '../../types';
+import {NETWORKS} from '../../constants';
 
 const TestStrategy = (props: { strategy: string, network: NETWORK, strategyOptions: any }) => {
     const [, showModal] = useModal();
     const [t] = useTranslation();
+    const [, showMessage] = useToast();
     const [inProgress, setInProgress] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(true);
     const address = useAddress(props.network);
@@ -68,6 +64,16 @@ const TestStrategy = (props: { strategy: string, network: NETWORK, strategyOptio
     };
 
     const handleTest = async () => {
+        if (isNaN(height)) {
+            showMessage(t('Invalid block height!'), 'error');
+            return;
+        }
+
+        if (!validateStacksAddress(userAddress)) {
+            showMessage(t('Invalid address!'), 'error');
+            return;
+        }
+
         setInProgress(true);
         setPower(null);
         let power: number | null = null;
@@ -82,7 +88,7 @@ const TestStrategy = (props: { strategy: string, network: NETWORK, strategyOptio
                 options: strategyOptions
             });
         } catch (e) {
-
+            showMessage(t("Couldn't get voting power"), 'error');
         } finally {
             setInProgress(false);
         }
@@ -130,11 +136,11 @@ const TestStrategy = (props: { strategy: string, network: NETWORK, strategyOptio
                                    }}
                         />
                     </Box>
-                    {power !== null && (
-                        <Box sx={{mb: '20px'}}>
-                            Voting power: {formatVotePower(power, space, 4)}
-                        </Box>
-                    )}
+                    <Box sx={{height: '30px'}}>
+                        {power !== null && (
+                            <> Voting power: {formatVotePower(power, space, 4)}</>
+                        )}
+                    </Box>
                 </ThemedBox>
             </DialogContent>
             <DialogActions>
