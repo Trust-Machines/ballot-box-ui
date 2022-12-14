05f1abfef4c80bb31d6f4e5774580f08ee7b490b
talhasch <talhabugrabulut@gmail.com>
2022-12-14 17:19:12 +0300

    logo image update

--- src/layout/app-header/components/brand.tsx
+++ src/layout/app-header/components/brand.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {useNavigate} from '@reach/router';
 import Box from '@mui/material/Box';
 
+import {useTheme} from '@mui/material';
 import useMediaBreakPoint from '../../../hooks/use-media-break-point';
 
 
 const Brand = () => {
     const [isSm] = useMediaBreakPoint();
     const navigate = useNavigate();
-
+    const theme = useTheme();
     return (
         <Box sx={{
             display: isSm ? 'flex' : 'none',
@@ -25,19 +26,15 @@ const Brand = () => {
                 <Box sx={{
                     width: '40px',
                     height: '40px',
-                    borderRadius: '50%',
                     overflow: 'hidden',
                     marginRight: '8px',
-                    background: '#F9A3A5',
+                    background: '#E5F2FF',
+                    border: `1px solid ${theme.palette.divider}`,
                     display: 'flex',
                     alignItems: 'center',
-                    justifyContent: 'center',
-                    'img': {
-                        width: '32px',
-                        height: '32px'
-                    }
+                    justifyContent: 'center'
                 }}>
-
+                    <Box component="img" src="/logo.png" sx={{width: '38px', height: '38px'}}/>
                 </Box>
                 <Box sx={{
                     fontSize: '1.3rem',
